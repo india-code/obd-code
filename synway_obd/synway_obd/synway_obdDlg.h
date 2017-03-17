b491e795ccb50aff86f49ae8f461ee744f9e03ed
krishna_dubey <krishna.dubey@spicedigital.in>
2017-03-17 11:04:02 +0530

    OBD Code initial commit

---
+++ synway_obd/synway_obd/synway_obdDlg.h
@@ -0,0 +1,34 @@
+
+// synway_obdDlg.h : header file
+//
+
+#pragma once
+
+
+// Csynway_obdDlg dialog
+class Csynway_obdDlg : public CDialogEx
+{
+// Construction
+public:
+	Csynway_obdDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SYNWAY_OBD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
